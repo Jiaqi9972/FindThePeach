484ce5f83737255867d874d73eccad9810696cb2
Jiaqi9972 <qu9972@gmail.com>
2024-08-06 01:46:04 -0700

    update serverless function to get posts

---
+++ src/app/api/getData/route.js
@@ -0,0 +1,23 @@
+export const dynamic = "force-dynamic";
+
+import { db } from "@/config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+export async function GET(request) {
+  try {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    const data = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,42 @@
 "use client";
+
 import { useEffect, useState } from "react";
 
 function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      console.log("fetch here");
-      const res = await fetch("/api/getData");
-      if (res.ok) {
-        const result = await res.json();
-        console.log(result);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/getPosts");
+        const result = await response.json();
         setData(result);
-      } else {
-        console.error("Failed to fetch data");
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-    }
+    };
     fetchData();
   }, []);
 
   return (
-    <main>
-      <h1>Hello, blog</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+    <main className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Hello, blog</h1>
+      {data.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        data.map((item) => (
+          <div key={item.id} className="bg-white shadow-md rounded-lg p-6 mb-4">
+            <h2 className="text-2xl font-semibold mb-2">{item.title}</h2>
+            <p className="text-gray-700 mb-4">{item.content}</p>
+            <p className="text-gray-500">
+              <strong>Tags:</strong>{" "}
+              {Array.isArray(item.tags)
+                ? item.tags.join(", ")
+                : "No tags available"}
+            </p>
+          </div>
+        ))
+      )}
     </main>
   );
 }
